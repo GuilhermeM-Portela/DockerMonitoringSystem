380dab46f6f8613c79744ef716fd262ce81825f4
GuilhermeM-Portela <guilhermemarquesportela@gmail.com>
2022-09-10 11:08:35 -0300

    adding environment preparation script

---
+++ upEnvironment.sh
@@ -0,0 +1,16 @@
+#Criando uma env
+echo "Criando a env.."
+python3 -m venv .env
+echo "env criada com sucesso!"
+
+#Ativando a env
+source .env/bin/activate
+
+echo "Instalando as dependencias..."
+pip3 install -r requirements.txt
+echo "Dependecias instaladas com sucesso!"
+
+#Subindo a API do eh
+echo "Iniciando serviço"
+uvicorn app.main:app --reload
+echo "Serviço iniciado.."
